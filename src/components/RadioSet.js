c4adfeed6742b7455f852bb50a997d46e9cf6b59
Stephanie Marchante <smarcha04@gmail.com>
2019-06-16 17:28:26 -0700

    refactor track component to use class and state

--- src/components/RadioSet.js
+++ src/components/RadioSet.js
@@ -3,26 +3,41 @@ import "./styles/RadioSet.css";
 
 import Playlist from './Playlist';
 
-const RadioSet = (props) => {
-  console.log(`Radio set for ${props.tracks.length} tracks`);
-  const playlists = {
-    morningTracks: props.tracks.slice(0, props.tracks.length / 2),
-    eveningTracks: props.tracks.slice(props.tracks.length / 2, props.tracks.length)
-  };
-  return (
-    <div className="radio-set">
-      <section className="radio-set--playlist-container">
-        <Playlist
-          side="Morning"
-          tracks={playlists.morningTracks}
-        />
-        <Playlist
-          side="Evening"
-          tracks={playlists.eveningTracks}
-        />
-      </section>
-    </div>
-  );
+class RadioSet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tracks: this.props.tracks
+    }
+  }
+
+  makeFavorite = (songId) => {
+    let updatedFavorite = this.state.tracks;
+    updatedFavorite
+  }
+
+
+  render() {
+    const playlists = {
+      morningTracks: props.tracks.slice(0, props.tracks.length / 2),
+      eveningTracks: props.tracks.slice(props.tracks.length / 2, props.tracks.length)
+    };
+
+    return (
+      <div className="radio-set">
+        <section className="radio-set--playlist-container">
+          <Playlist
+            side="Morning"
+            tracks={playlists.morningTracks}
+          />
+          <Playlist
+            side="Evening"
+            tracks={playlists.eveningTracks}
+          />
+        </section>
+      </div>
+    );
+  }
 };
 
 export default RadioSet;
--- src/components/Track.js
+++ src/components/Track.js
@@ -5,35 +5,45 @@ import "./styles/Track.css";
 
 // Here we use destructuring to extract the props into separate variables
 // See https://wesbos.com/destructuring-objects/
-const Track = ({title, artist, playtime, albumart, favorite}) => {
-  const markFavorite = () => {
+class Track extends React.Component { 
+  state = {
+    favorite: true,
+  }
+  
+  onMarkFavorite = () => {
     console.log('I\'m a favorite now!');
   }
-  return (
-    <li className="track">
-      <img className="track--albumart" alt={`album art for ${title}`} src={albumart} />
-      <h3 className="track--title">{title}</h3>
-      <input
-        type="checkbox"
-        className="track--favorite"
-        checked={!favorite}
-        onChange={ markFavorite }
-      />
-      <p className="track--artist">{artist}</p>
-      <p className="track--playtime">{playtime}</p>
-      <button
-        className="track--control track--to-top"
-        >
-        <span role="img" aria-label="send to top">🔝</span>
-      </button>
-      <button
-        className="track--control track--switch"
-        >
-        <span role="img" aria-label="switch lists">↔</span>
-      </button>
-    </li>
-  );
-};
+
+  render() {
+    const { title, artist, playtime, albumart } = this.props
+    
+
+    return (
+      <li className="track">
+        <img className="track--albumart" alt={`album art for ${title}`} src={albumart} />
+        <h3 className="track--title">{title}</h3>
+        <input
+          type="checkbox"
+          className="track--favorite"
+          checked={this.state.favorite}
+          onChange={ this.onMarkFavorite }
+        />
+        <p className="track--artist">{artist}</p>
+        <p className="track--playtime">{playtime}</p>
+        <button
+          className="track--control track--to-top"
+          >
+          <span role="img" aria-label="send to top">🔝</span>
+        </button>
+        <button
+          className="track--control track--switch"
+          >
+          <span role="img" aria-label="switch lists">↔</span>
+        </button>
+      </li>
+    );
+  };
+}
 
 Track.propTypes = {
   title: PropTypes.string,
